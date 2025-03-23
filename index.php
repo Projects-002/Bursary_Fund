765e873a09ed6cc236f9482fb21f1783bda5b50c
Aleks <alexkipchirchir927@gmail.com>
2025-03-23 11:43:38 +0300

    updates

--- index.php
+++ index.php
@@ -624,7 +624,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                    &copy; <a class="border-bottom" href="#">alexkipchirchir, All Right Reserved.
+                    &copy; <a class="border-bottom">alexkipchirchir, All Right Reserved.
                 </div>
                 <div class="col-md-6 text-center text-md-end">
                     <!--/*** This  is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the  without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
